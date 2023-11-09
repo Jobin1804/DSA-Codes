feb1c50ea67b269f6318c7bcafa4eb2128bd4421
Jobin1804 <144207696+Jobin1804@users.noreply.github.com>
2023-11-09 11:34:56 +0530

    Add files via upload

---
+++ DSA Program 1.c
@@ -0,0 +1,36 @@
+/*
+Roll no:24
+Batch:A
+Author Name:Jobin Abraham
+Date:2/09/23
+Description:determine if no is in fibonacci sequence
+*/
+#include <stdio.h>
+  int main() {
+        int data, num1 = 0, num2 = 1, temp, flag = 0;
+
+        printf("Enter ur input:");
+        scanf("%d", &data);
+
+        if (data == num1 || data == num2) {
+                printf("%d is a fibonacci number\n", data);
+                return 0;
+        }
+
+        while (num2 <= data) {
+                temp = num2;
+                num2 = num1 + num2;
+                num1 = temp;
+                if (num2 == data) {
+                        flag = 1;
+                        break;
+                }
+        }
+
+        if (flag) {
+                printf("%d is a fibonacci number\n", data);
+        } else {
+                printf("%d is not a fibonacci number\n", data);
+        }
+        return 0;
+  }
---
+++ DSA Program 2.c
@@ -0,0 +1,36 @@
+/*
+Roll no:24
+Batch:A
+Author Name:Jobin Abraham
+Date:2/09/23
+Description:pascal triangle
+*/
+#include <stdio.h>
+
+int main()
+{
+    int arr[50][50];
+
+    int i = 0;
+    int j = 0;
+
+    int n = 0;
+
+    printf("Enter the number of lines: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n - 1 - i; ++j)
+            printf(" ");
+
+        for (j = 0; j <= i; ++j) {
+            if (j == 0 || j == i)
+                arr[i][j] = 1;
+            else
+                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
